be9caa4f859d9707053c40dcc7d62fc4b5cafe3d
Paul <p.karpukhin@ltst.ru>
2020-02-17 22:53:24 +0600

    Remove imports

--- presentation/screens/search/src/main/java/com/raiden/search/models/Change.kt
+++ presentation/screens/search/src/main/java/com/raiden/search/models/Change.kt
@@ -1,7 +1,5 @@
 package com.raiden.search.models
 
-import com.raiden.domain.models.User
-
 sealed class Change {
     object Idle : Change()
     data class ShowContent(val users: List<UserViewModel>): Change()
